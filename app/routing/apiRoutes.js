b8acbdf0cbd4a7859a8fb34744486f79a1790853
TimRCAnderson <timothyrcanderson@gmail.com>
2017-05-20 06:42:06 -0400

    Need to fix post API call (scope issue, it seems), and set up the modal window to display the returned friend.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var path = require("path");
 var fs = require("fs");
 
 router.get("/api/friends", function(req, res) {
@@ -7,10 +8,28 @@ router.get("/api/friends", function(req, res) {
 });
 
 router.post("/api/friends", function(req, res) {
-  friendArray.push(req.body);
-  console.log(req.body);
+  var newbie = req.body;
+  var bestFriend = {};
+  var bestCompatibility = 50;
+  console.log(router.friendArray);
+  for(var i = 0; i < router.friendArray.length; i++)
+  {
+    var compat = 0;
+    for(var j = 0; j < 10; i++)
+    {
+      compat += Math.abs(router.friendArray[i].scores[j] - newbie.scores[j]);
+    }
+    if(compat < bestCompatibility)
+    {
+      bestFriend = router.friendArray[i];
+      bestCompatibility = compat;
+    }
+  }
+  res.json(bestFriend);
+  router.friendArray.push(newbie);
+  console.log(newbie);
   console.log(path.join(__dirname, "../data/friends.js"));
-  fs.writeFile(path.join(__dirname, "../data/friends.js"), JSON.stringify(friendArray), function(err) {
+  fs.writeFile(path.join(__dirname, "../data/friends.js"), JSON.stringify(router.friendArray), function(err) {
     if(err) throw err;
   });
 });
